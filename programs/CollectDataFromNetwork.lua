a0f3b44804303f631173c7b6f9e7f4a35133a383
jultomtenreal <68858896+jultomtenreal@users.noreply.github.com>
2022-05-08 19:26:59 +0200

    Added CollectDataFromNetwork.lua

---
+++ programs/CollectDataFromNetwork.lua
@@ -0,0 +1,27 @@
+local component = require("component")
+local m = component.modem
+assert(modem, "You are fucking stupid")
+if not modem.isOpen(69) then
+  print("Do you want to open port 69? [y/n]")
+  local option = io.read(1):lower()
+  if option == "y" then
+    print("Opening port 69")
+    modem.open(69)
+  else
+    return
+  end
+end
+if not modem.isOpen(69) then
+  return
+end
+print("Port 69 is open!")
+
+os.sleep(1)
+print("Want to look for messages? [y/n]")
+local option = io.read(1)
+if option == "y" then
+  while true do
+  local _, _, from, port, _, message = event.pull("modem_message")
+print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
+  end
+end 
