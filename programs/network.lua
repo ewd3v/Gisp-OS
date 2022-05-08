0ddae8bb2d0772817b73ed356a09d8775904e631
jultomtenreal <68858896+jultomtenreal@users.noreply.github.com>
2022-05-08 19:10:00 +0200

    Added broadcast test message

--- programs/network.lua
+++ programs/network.lua
@@ -2,7 +2,7 @@ local component = require("component")
 local modem = component.modem
 assert(modem, "You are fucking stupid")
 if not modem.isOpen(69) then
-  print("Do you want to open port 69?")
+  print("Do you want to open port 69? [y/n]")
   local option = io.read(1):lower()
   if option == "y" then
     print("Opening port 69")
@@ -16,5 +16,15 @@ if not modem.isOpen(69) then
 end
 
 print("Port 69 is open!")
-
-local input = io.read()
+os.sleep(1)
+print("Want to send a test message every 5 second? [y/n]")
+if option == "y" then
+  while true do
+  if broadcast(69, "TEST") then
+    print("Sent the message")
+    else
+    print("Failed to send the message")
+    end
+    os.sleep(5)
+  end
+end 
