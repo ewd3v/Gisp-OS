df2d54219a98f79f5bea0559c810d4ab88449206
jultomtenreal <68858896+jultomtenreal@users.noreply.github.com>
2022-05-09 16:47:05 +0200

    Create DownloadAndTransfer.lua

---
+++ programs/DownloadAndTransfer.lua
@@ -0,0 +1,37 @@
+os.execute("clear")
+local event = require("event")
+local component = require("component")
+local modem = component.modem
+assert(modem, "You are fucking stupid")
+if not modem.isOpen(420) then
+  print("Do you want to open port 420? [y/n]")
+  local option = io.read():lower()
+  if option == "y" then
+    print("Opening port 420")
+    modem.open(420)
+  else
+    return
+  end
+end
+if not modem.isOpen(420) then
+  return
+end
+
+print("Port 420 is open!")
+os.sleep(1)
+os.execute("clear")
+print("Is this the downloading server? [y/n]")
+local option = io.read()
+if option == "y" then
+  while true do
+    local _, _, _, _, _, message = event.pull("modem_message")
+    local args = string.split(message, " ")
+    local command = args[1]
+    table.remove(args, 1)
+    if command == "get" then
+      local raw_file = args[1]
+      local internet = require("internet")
+      local handle = internet.request(raw_file)
+      local result = ""
+      for chunk in handle do result = result..chunk end
+      
