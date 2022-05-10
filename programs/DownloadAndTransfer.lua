1d7e9564569eb391d75de9f3a3be11a61193290d
jultomtenreal <jultomtenriktig@gmail.com>
2022-05-10 21:32:23 +0200

    Remade most of DownloadAndTransfer

--- programs/DownloadAndTransfer.lua
+++ programs/DownloadAndTransfer.lua
@@ -26,29 +26,59 @@ if option == "y" then
     print("This servers ip is: " .. own_ip)
   while true do
     local _, _, ip, _, _, message = event.pull("modem_message")
-    print("Got a request from: " .. ip)
-      local raw_file = message
-      local internet = require("internet")
-      local handle = internet.request(raw_file)
-      local result = ""
-      for chunk in handle do result = result..chunk end
-      if modem.send(ip, 420, result) then
-        print("Sent the file")
-        else
-        print("Failed to send the file")
-      end
-  end
-  else
-    print("enter the servers ip")
-      local server_ip = io.read()
-    print("Enter your download request (Input your link)")
-      local link = io.read()
-    modem.send(server_ip, 420, link)
-    local _, _, ip, _, _, file = event.pull("modem_message")
-    if ip == server_ip then
-    local io = require("io")
-    local f = io.open("RENAME_DOWNLOADED_FILE.lua","w")
-    f:write(file)
-    f:close()
+    if message == "Server? Do you exsist?" then
+    print("Responding to search from: " .. ip)
+    if modem.send(ip, 420, "Yes I exsist") then
+      print("Responded to search!")
+      local _, _, ip2, _, _, message = event.pull("modem_message")
+        if ip2 == ip then
+        local raw_file = message
+        local internet = require("internet")
+        assert(internet, "You are fucking stupid")
+        local handle = internet.request(raw_file)
+        local result = ""
+        for chunk in handle do result = result..chunk end
+        if modem.send(ip, 420, result) then
+          print("Sent the file")
+          else
+          print("Failed to send the file")
+        end
+      else
+        print("Failed to responde to: " .. ip)
+    end
     end
   end
+
+
+  else
+    os.execute("clear")
+    print("Searching for server")
+      if modem.broadcast(420, "Server? Do you exsist?") then
+        while(true)
+        local _, _, ip, _, _, exsist = event.pull("modem_message") then
+          if exsist == "Yes I exsist" then
+            os.execute("clear")
+            print("Found a server")
+            print("Enter your download request (Input your link)")
+            local link = io.read()
+            if modem.send(ip, 420, link) then
+              os.execute("clear")
+              print("Sent your request, waiting for server to send your file!")
+              local _, _, ip2, _, _, file = event.pull("modem_message")
+            if ip2 == ip then
+              print("Choose file format (.lua is the most common)")
+              local format = io.read()
+              local f = io.open("RENAME_DOWNLOADED_FILE.lua","w")
+              f:write(file)
+              f:close()
+            end
+          end
+            else
+              os.execute("clear")
+              print("Failed to send request :/")
+            end
+          end
+      else
+        print("Failed to search for servers")
+      end
+
